55b169b8c933c08c3b493928b2bc1146284dddb0
josephcarmack <joseph.liping@gmail.com>
2018-04-30 16:42:11 -0500

    basic code outline/initial commit

---
+++ pyPhase.py
@@ -0,0 +1,11 @@
+'''A very simple phase-field code that numerically solves the Cahn-Hilliard equation.'''
+
+import numpy as np
+
+class InputParams:
+    '''class for storing phase-field input parameters.'''
+    pass
+
+class scalarField:
+    '''An implementation of a simple scalar phase-field order parameter'''
+    pass
